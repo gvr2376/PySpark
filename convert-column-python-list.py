2a5e4685417ca13a2599ba0d3b6c701f9e0d5f25
Kevin Luzbetak <fdxowye@gmail.com>
2021-12-27 22:33:02 -0800

    Bug fix

--- convert-column-python-list.py
+++ convert-column-python-list.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Convert column to list
+Convert column to List
 """
 
 from pyspark.sql import SparkSession
@@ -9,11 +9,9 @@ spark = SparkSession.builder.master("local[1]") \
     .appName('SparkByExamples.com') \
     .getOrCreate()
 
-data = [
-        ("James", "Smith", "USA", "CA"), ("Michael", "Rose", "USA", "NY"),
+data = [("James", "Smith", "USA", "CA"), ("Michael", "Rose", "USA", "NY"),
         ("Robert", "Williams", "USA", "CA"), ("Maria", "Jones", "USA", "FL")
-       ]
-
+        ]
 columns = ["firstname", "lastname", "country", "state"]
 df = spark.createDataFrame(data=data, schema=columns)
 df.show()
