878def0a9d9d1e6a397202555c5fd421233011e7
Kevin Luzbetak <fdxowye@gmail.com>
2021-12-27 22:54:25 -0800

    Data Aggregation

--- convert-column-python-list.py
+++ convert-column-python-list.py
@@ -5,12 +5,16 @@ Convert column to List
 
 from pyspark.sql import SparkSession
 
+
 spark = SparkSession.builder.master("local[1]") \
     .appName('SparkByExamples.com') \
     .getOrCreate()
 
-data = [("James", "Smith", "USA", "CA"), ("Michael", "Rose", "USA", "NY"),
-        ("Robert", "Williams", "USA", "CA"), ("Maria", "Jones", "USA", "FL")
+data = [
+            ("James", "Smith", "USA", "CA"),
+            ("Michael", "Rose", "USA", "NY"),
+            ("Robert", "Williams", "USA", "CA"),
+            ("Maria", "Jones", "USA", "FL")
         ]
 columns = ["firstname", "lastname", "country", "state"]
 df = spark.createDataFrame(data=data, schema=columns)
